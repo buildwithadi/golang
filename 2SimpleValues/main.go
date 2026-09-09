a00ba86263ef45c03f06dcf51adbb65c9f284789
buildwithadi <adityarawat.online@gmail.com>
2026-09-09 22:24:11 +0530

    simple values

---
+++ 2SimpleValues/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// integer
+	fmt.Println(1)
+	// string
+	fmt.Println("Hello World")
+	// boolean
+	fmt.Println(true)
+	fmt.Println(false)
+	// float
+	fmt.Println(3.14)
+}
