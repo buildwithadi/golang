b243bbb9411bfaf47e5ff9b1308d6fec23b8447b
buildwithadi <adityarawat.online@gmail.com>
2026-09-10 22:35:14 +0530

    variable

---
+++ 3Variable/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "aditya"
+	var language string
+	var lastName = " rawat"
+	pi := 3.1415
+	age := 20
+	isAdult := true
+	language = "english"
+
+	fmt.Println(name + lastName)
+	fmt.Println(age)
+	fmt.Println(isAdult)
+	fmt.Println(language)
+	fmt.Println(pi)
+}
